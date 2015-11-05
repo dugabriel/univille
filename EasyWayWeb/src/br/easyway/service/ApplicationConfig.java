f306367ea83688ac49a07ee854cf2ce7c8d1061b
Eduardo Gabriel <eduardofranciscogabriel@gmail.com>
2015-11-05 09:37:30 -0200

    teste

---
+++ EasyWayWeb/src/br/easyway/service/ApplicationConfig.java
@@ -0,0 +1,19 @@
+package br.easyway.service;
+import java.util.Set;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+ 
+@ApplicationPath("/rest")
+public class ApplicationConfig extends Application {
+    @Override
+    public Set<Class<?>> getClasses() {
+        return getRestResourceClasses();
+    }
+     
+    private Set<Class<?>> getRestResourceClasses() {
+        Set<Class<?>> resources = new java.util.HashSet<Class<?>>();
+        resources.add(CustomerService.class);
+        return resources;
+    }
+}
---
+++ EasyWayWeb/src/br/easyway/service/CustomerService.java
@@ -0,0 +1,28 @@
+package br.easyway.service;
+
+import java.util.List;
+
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+import br.easyway.ejb.CustomerBeanLocal;
+import br.easyway.entity.Customer;
+
+@Path("/customer")
+@javax.enterprise.context.RequestScoped
+public class CustomerService {
+	
+	@Inject
+	private CustomerBeanLocal customerBean;
+	
+	@GET
+	@Path("/all")
+	@Produces("application/json")
+	public List<Customer> getAllCustomer(){
+		return customerBean.getAllCustomer();
+	}
+	
+	
+}
